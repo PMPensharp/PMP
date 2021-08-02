2c040c3bf89f698c2290a1fe29e7e4cc85ff0a8b
김성욱 <xsungwook3x@naver.com>
2021-08-03 01:56:14 +0900

    [start]

--- app/src/main/java/com/example/pmp/MainActivity.java
+++ app/src/main/java/com/example/pmp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.pmp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private Fragment1 fragment1 = new Fragment1();
+    private Fragment2 fragment2 = new Fragment2();
+    private Fragment3 fragment3 = new Fragment3();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment1).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.icon1:
+                    transaction.replace(R.id.frameLayout, fragment1).commitAllowingStateLoss();
+
+                    break;
+                case R.id.icon2:
+                    transaction.replace(R.id.frameLayout, fragment2).commitAllowingStateLoss();
+                    break;
+                case R.id.icon3:
+                    transaction.replace(R.id.frameLayout, fragment3).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 }
